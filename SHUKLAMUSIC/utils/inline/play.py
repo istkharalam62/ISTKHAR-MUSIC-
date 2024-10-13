3002aa419df36fab1bdcdc9d67e18fae6faaadcc
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-10-13 08:10:11 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -63,9 +63,9 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),        ],
         [
          InlineKeyboardButton(text="🫧 ᴏᴡɴᴇʀ 🫧", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="🍁 ɢʀᴏᴜᴘ 🍁", url=f"{SUPPORT_CHAT}",),
