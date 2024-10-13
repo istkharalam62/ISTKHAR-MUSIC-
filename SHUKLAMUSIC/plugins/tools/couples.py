29e98df46164e3fa0e1d1c4f212c53ddd504976c
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-10-13 08:06:09 +0530

    Update couples.py

--- SHUKLAMUSIC/plugins/tools/couples.py
+++ SHUKLAMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SHUKLAMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗪𝗢𝗥𝗟𝗗 ]彡𒆜𓊉",
-            url=f"https://t.me/MASTIWITHFRIENDSXD",
+            text="𓊈𒆜彡[ 𓆰⎯꯭꯭‌༎𝅃꯭꯭᳚]꯭ ⃪꯭ 𝐈꯭꯭ѕ꯭꯭፝֠֩‌тк꯭꯭н፝֠֩‌α꯭꯭я꯭꯭𝅃꯭᳚‐꯭꯭»꯭꯭.꯭⟶᯦꯭꯭꯭🇮🇳꯭꯭〞 ]彡𒆜𓊉",
+            url=f"https://t.me/+uxbw-Rr1fVI5ZTc1",
         ),
     ],
 ]
